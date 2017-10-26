63ce7b2dc5f703d6dd19e81fca430fb61880dbc7
felipe-gouveia <felipegouveia3@gmail.com>
2017-10-26 05:48:15 -0300

    Initial Commit
    Added archives for Real Time Objects Recognizer

---
+++ ComputacaoGrafica/Avaliacao01/RealTimeObjectsRecognizer/app/src/main/java/br/belem/felipegouveia/realtimeobjectsrecognizer/MainActivity.java
@@ -0,0 +1,185 @@
+package br.belem.felipegouveia.realtimeobjectsrecognizer;
+
+        import android.Manifest;
+        import android.content.pm.PackageManager;
+        import android.graphics.Bitmap;
+        import android.os.Bundle;
+        import android.os.SystemClock;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v4.content.ContextCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.text.method.ScrollingMovementMethod;
+        import android.util.Log;
+        import android.view.SurfaceView;
+        import android.widget.TextView;
+
+        import org.opencv.android.BaseLoaderCallback;
+        import org.opencv.android.CameraBridgeViewBase;
+        import org.opencv.android.JavaCameraView;
+        import org.opencv.android.LoaderCallbackInterface;
+        import org.opencv.android.OpenCVLoader;
+        import org.opencv.android.Utils;
+        import org.opencv.core.CvType;
+        import org.opencv.core.Mat;
+        import org.opencv.core.Size;
+
+        import java.util.List;
+        import java.util.concurrent.Executor;
+        import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    private static final int INPUT_SIZE = 224;
+    private static final int IMAGE_MEAN = 117;
+    private static final float IMAGE_STD = 1;
+    private static final String INPUT_NAME = "input";
+    private static final String OUTPUT_NAME = "output";
+
+    private static final String MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb";
+    private static final String LABEL_FILE =
+            "file:///android_asset/imagenet_comp_graph_label_strings.txt";
+
+    // OPENCV DECLARATIONS
+    private JavaCameraView mOpenCvCameraView;
+    Mat mRgba, mGray;
+    Bitmap mBitmap;
+
+    private Classifier classifier;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private TextView textViewResult;
+
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+
+                case BaseLoaderCallback.SUCCESS:{
+                    mOpenCvCameraView.enableView();
+                    break;
+                }
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+            super.onManagerConnected(status);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},1);
+        }
+
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        textViewResult.setMovementMethod(new ScrollingMovementMethod());
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        initTensorFlowAndLoadModel();
+        SystemClock.sleep(3000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCv has failed to initiate.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mLoaderCallback);
+        } else {
+            Log.i(TAG, "OpenCV initiated successfuly");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(mOpenCvCameraView!=null)
+            mOpenCvCameraView.disableView();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mOpenCvCameraView!=null)
+            mOpenCvCameraView.disableView();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                classifier.close();
+            }
+        });
+    }
+
+    private void initTensorFlowAndLoadModel() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    classifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_FILE,
+                            LABEL_FILE,
+                            INPUT_SIZE,
+                            IMAGE_MEAN,
+                            IMAGE_STD,
+                            INPUT_NAME,
+                            OUTPUT_NAME);
+                } catch (final Exception e) {
+                    throw new RuntimeException("Error initializing TensorFlow!", e);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC3);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        Size sizeGray = mRgba.size();
+        int rowss = (int) sizeGray.height;
+        int colss = (int) sizeGray.width;
+
+        Mat tmp =  new Mat(colss, rowss, CvType.CV_8UC3);
+
+        mBitmap = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mRgba, mBitmap);
+
+        //mBitmap = BitmapFactory.decodeByteArray(mBitmap, 0, mBitmap.width);
+
+        mBitmap = Bitmap.createScaledBitmap(mBitmap, INPUT_SIZE, INPUT_SIZE, false);
+
+        final List<Classifier.Recognition> results = classifier.recognizeImage(mBitmap);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textViewResult.setText(results.toString());
+                Log.i(TAG, results.toString());
+            }
+        });
+
+        return mRgba;
+    }
+}
